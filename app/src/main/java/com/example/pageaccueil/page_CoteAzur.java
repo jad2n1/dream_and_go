8f50651ba26222212f8686b035b0cc36c2863e64
jadedehaynin <jade.dehaynin@epfedu.fr>
2022-05-05 21:35:20 +0200

    création bouton retour page cote d azur à choix destination

--- app/src/main/java/com/example/pageaccueil/page_CoteAzur.java
+++ app/src/main/java/com/example/pageaccueil/page_CoteAzur.java
@@ -1,12 +1,16 @@
 package com.example.pageaccueil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class page_CoteAzur extends AppCompatActivity {
+    Button Bouton_CoteAzur_retour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,15 @@ public class page_CoteAzur extends AppCompatActivity {
         // 4) Association de adapter au spinner
         monSpinner4.setAdapter(monAdapter);
 
-        // exemple de commentaire vide
+        Bouton_CoteAzur_retour = (Button) findViewById(R.id.btn_retour_coteazur);
+
+        Bouton_CoteAzur_retour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // ouverture de l'activité
+                Intent intent = new Intent(page_CoteAzur.this, choix_destination.class);
+                startActivity(intent);
+            }
+        });
     }
 }
