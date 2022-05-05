e47ed2117954b65e7a63786da963838e49bc5f4f
jadedehaynin <jade.dehaynin@epfedu.fr>
2022-05-05 20:47:22 +0200

    creation spinner alpes

--- app/src/main/java/com/example/pageaccueil/page_Alpes.java
+++ app/src/main/java/com/example/pageaccueil/page_Alpes.java
@@ -1,4 +1,31 @@
 package com.example.pageaccueil;
 
-public class page_Alpes {
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class page_Alpes extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alpes_page);
+
+        // 1) creation d’un objet-reference  spinner vers le spinner de l’activite
+        Spinner monSpinner3 = (Spinner) findViewById(R.id.spinner_jours3);
+
+// 2) creation d’un ArrayAdapter à partir de tab_pokemon et mise en forme par defaut
+        ArrayAdapter<CharSequence> monAdapter = ArrayAdapter.createFromResource (this,
+                R.array.Jours, android.R.layout.simple_spinner_item);
+
+        // 3) définition de la mise en page
+        monAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // 4) Association de adapter au spinner
+        monSpinner3.setAdapter(monAdapter);
+
+        // exemple de commentaire vide
+    }
 }
