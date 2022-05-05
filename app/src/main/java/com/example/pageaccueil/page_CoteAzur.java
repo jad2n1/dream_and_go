d583ab35880d6d605d2cd3f5291b8b40547d55ce
jadedehaynin <jade.dehaynin@epfedu.fr>
2022-05-05 20:59:42 +0200

    creation spinner cote azur

--- app/src/main/java/com/example/pageaccueil/page_CoteAzur.java
+++ app/src/main/java/com/example/pageaccueil/page_CoteAzur.java
@@ -1,4 +1,31 @@
 package com.example.pageaccueil;
 
-public class page_CoteAzur {
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class page_CoteAzur extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.coteazur_page);
+
+        // 1) creation d’un objet-reference  spinner vers le spinner de l’activite
+        Spinner monSpinner4 = (Spinner) findViewById(R.id.spinner_jours4);
+
+// 2) creation d’un ArrayAdapter à partir de tab_pokemon et mise en forme par defaut
+        ArrayAdapter<CharSequence> monAdapter = ArrayAdapter.createFromResource (this,
+                R.array.Jours, android.R.layout.simple_spinner_item);
+
+        // 3) définition de la mise en page
+        monAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // 4) Association de adapter au spinner
+        monSpinner4.setAdapter(monAdapter);
+
+        // exemple de commentaire vide
+    }
 }
