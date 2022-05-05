f3707b88b62e709a8311250e3ca9b7f9a1ba906a
jadedehaynin <jade.dehaynin@epfedu.fr>
2022-05-05 20:36:56 +0200

    creation spinner atlantique

--- app/src/main/java/com/example/pageaccueil/page_Atlantique.java
+++ app/src/main/java/com/example/pageaccueil/page_Atlantique.java
@@ -1,4 +1,31 @@
 package com.example.pageaccueil;
 
-public class page_Atlantique {
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class page_Atlantique extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.atlantique_page);
+
+        // 1) creation d’un objet-reference  spinner vers le spinner de l’activite
+        Spinner monSpinner2 = (Spinner) findViewById(R.id.spinner_jours2);
+
+// 2) creation d’un ArrayAdapter à partir de tab_pokemon et mise en forme par defaut
+        ArrayAdapter<CharSequence> monAdapter = ArrayAdapter.createFromResource (this,
+                R.array.Jours, android.R.layout.simple_spinner_item);
+
+        // 3) définition de la mise en page
+        monAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // 4) Association de adapter au spinner
+        monSpinner2.setAdapter(monAdapter);
+
+        // exemple de commentaire vide
+    }
 }
