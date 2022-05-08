24fc36110b6cdbae57e87b0ebfebed004435b0a3
jeanne <jeanne.ginzburg@epfedu.fr>
2022-05-08 20:38:41 +0200

    dans la page Cartes afficher alpes si bouton cliquer depuis Alpes

--- app/src/main/java/com/example/pageaccueil/Carte.java
+++ app/src/main/java/com/example/pageaccueil/Carte.java
@@ -18,7 +18,9 @@ public class Carte extends AppCompatActivity {
         // association de nos references avec les objets du layout
         Carte_Alpes = ( Button ) findViewById(R.id.btn_carte);
         TextView  Nom_destination = (TextView) findViewById(R.id.nom_destination);
-String carte=getIntent().getStringExtra("nom_cartes");
+            Bundle extra=getIntent().getExtras();
+
+String carte = extra.getString("carte");
 
 
         if (carte.equals("Alpes")){
