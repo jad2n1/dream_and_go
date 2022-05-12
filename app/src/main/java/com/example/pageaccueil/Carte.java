795aeaf02ba387f7a42cb8d1974fe3c423ac7124
jeanne <jeanne.ginzburg@epfedu.fr>
2022-05-12 17:48:48 +0200

    commentaire page Carte

--- app/src/main/java/com/example/pageaccueil/Carte.java
+++ app/src/main/java/com/example/pageaccueil/Carte.java
@@ -24,15 +24,12 @@ public class Carte extends AppCompatActivity {
 getSupportActionBar().hide(); // permet d'enlever la bar d'en haut
 
         // association de nos references avec les objets du layout
-      //  Carte_Alpes = ( Button ) findViewById(R.id.btn_carte);
+
         TextView  Nom_destination = (TextView) findViewById(R.id.nom_destination);
             Bundle extra=getIntent().getExtras();
-            String finalBtn_Carte = extra.getString("carte");
-
-
-// affichage dans le text view le nom de la destination
-        Carte= findViewById(R.id.imageView_cartw_chamonix);
-        Bouton_retour = (Button) findViewById(R.id.btn_retour_carte);
+            String finalBtn_Carte = extra.getString("carte"); // recoit la valeur de "carte"
+        // à partir des pages Paris ou Atlantique ou Alpes ou cotes d'Azur
+        // affichage dans le text view le nom de la destination grace au nom recut
 
             Nom_destination.setText(finalBtn_Carte);
 
@@ -40,10 +37,12 @@ getSupportActionBar().hide(); // permet d'enlever la bar d'en haut
 
             switch (Nom_destination.getText().toString()){
                 case "Alpes":
-                    Carte.setImageResource(R.drawable.carte_chamonix);
-                    Carte.setVisibility(View.VISIBLE);
-                    break;
-
+                    Carte.setImageResource(R.drawable.carte_chamonix); // l'image Carte
+                    // qui correspond à la carte des Alpes est récupérée grace à son id dans le
+                    // xml (touites les images "cartes_" sont invisbles)
+                    Carte.setVisibility(View.VISIBLE); // puis la carte en question est rendu visible
+                    break; // pour sortir du switch
+// meme fonctionnement pour les autres cas
                 case "Atlantique":
 
                     Carte_Atlantique=findViewById(R.id.imageView_Atlantique_carte);
@@ -63,6 +62,8 @@ getSupportActionBar().hide(); // permet d'enlever la bar d'en haut
     }
 }
 
+        // bouton retour
+        Bouton_retour = (Button) findViewById(R.id.btn_retour_carte);
         Bouton_retour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
